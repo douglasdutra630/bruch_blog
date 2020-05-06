0717a2932c5b35cb76f0ce9213b23c77ef43a137
douglasdutra630 <douglas.dutra630@gmail.com>
2020-05-06 13:39:20 -0600

    starting api requests for blog

--- src/components/pages/coding.js
+++ src/components/pages/coding.js
@@ -1,6 +1,33 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Coding extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      blogItems: [],
+    };
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    /*API POINT NEEDED */ axios
+      .get("", { withCredentials: true })
+      .then((res) => {
+        this.setState({
+          blogItems: [] /*needs res.data.(whatever the api brings back)*/,
+        });
+      })
+      .catch((err) => {
+        console.log("getBlogItems error", err);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
   render() {
     return (
       <div className="app">
